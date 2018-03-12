d8eb94ba781b420772eeb27fe70815bc2e6cfb43
Elshad Seyidmammadov <elshad@eclipsesource.com>
2018-03-12 15:47:01 +0400

    Add a Paid Flavor
    Step 5: Add a Paid Flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import com.elshadsm.custom.androidlibrary.activities.JokeViewActivity;
 import com.elshadsm.custom.jokelib.JokeProvider;
 
 
